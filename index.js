14a7fa2f718506d36fe167e4a2ddc4f748d089f6
Berard <louis.berard@devinci.fr>
2018-03-16 19:15:15 +0100

    End

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-const express        = require('express');
-const app            = express();
+const express = require('express');
+const app = express();
 const port = 9292;
+const {
+  getModels
+} = require('node-car-api');
+const {
+  getBrands
+} = require('node-car-api');
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 
+//function to bulk
+function bulking(theString) {
+  client.bulk({
+    body: [theString]
+  });
+}
 
 //populate
-const {getModels} = require('node-car-api');
-
 var json = '';
 var id = 0;
 var fs = require('fs');
-async function print () {
-  const models = await getModels('AUDI');
-  models.forEach(function(element){
-    json += '{ "index":{ "_id": "'+id+'"} }\n';
-    json += JSON.stringify(element);
-    json += '\n';
-    id++;
-  });
-  console.log(json);
-fs.writeFile("resultat.json", json, function(err) {
+async function populate() {
+  var brands = await getBrands();
+  for(var i=0; i < brands.length; i++) {
+    var brand = brands[i];
+    var models = await getModels(brand);
+    models.forEach(function(element) {
+      json += '{ "index":{ "_index": "suv", "_type":"suv", "_id": "' + id + '"} }\n';
+      json += JSON.stringify(element);
+      json += '\n';
+      id++;
+    });
+  };
+  console.log('test1');
+  fs.writeFile("resultat.json", json, function(err) {
     if (err) {
-        console.log(err);
+      console.log(err);
     }
-});
+  });
+  bulking(json);
 }
+populate();
 
 //routes
 app.get('/populate', (req, res) => {
-  res.send('Hello')
+  populate
+  res.send('Populate completed')
 });
 
 app.get('/suv', (req, res) => {
